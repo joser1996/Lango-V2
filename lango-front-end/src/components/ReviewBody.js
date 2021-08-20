2265342b1942e1d661afc999d22d916a68aac298
Jose Torres <josertrs@gmail.com>
2021-08-19 17:17:31 -0700

    Toggling between review and add pages. Added CSS. Need to fix css layout for review page

--- lango-front-end/src/components/ReviewBody.js
+++ lango-front-end/src/components/ReviewBody.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import styles from '../pages/HomePage.module.css';
 
 export default class ReviewBody extends Component {
+P
+    checkAns = () => {
+        console.log("Checking answer");
+    };
+
     render() {
         return (
-            <div className="reviewContainer">
-                
-            </div>
+            <main id={styles.langoMain}>
+                <div id={styles.reviewTextCard}>
+                    <p id="pReview"></p>
+                </div>
+
+                <div id={styles.reviewInputCard}>
+                    <textarea 
+                        id="reviewTextArea" 
+                        defaultValue="Hello"
+                        onKeyPress={this.checkAns}>
+                    </textarea>
+                </div>
+                <div id={styles.nextButtonDiv}>
+                    <button id={styles.nextButton}></button>             
+                </div>
+            </main>
         )
     }
 }
